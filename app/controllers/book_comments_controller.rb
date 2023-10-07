deb08da0428edece4fbeba81d2c38a8df19bb869
ryokatakahashi <ryonryon.xxxx@gmail.com>
2023-10-07 11:32:15 +0000

    修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,15 +2,15 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    if book_comment
-    book_comment.destroy
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    if @comment
+    @comment.destroy
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,24 +7,24 @@ Rails.application.routes.draw do
   get 'home/about', to: 'homes#about', as: 'about'
 
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
-    resource :book_comments, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-    delete '/book_favorites', to: 'favorites#destroy', as: 'book_favorite'
+    #delete '/book_favorite', to: 'favorites#destroy', as: 'book_favorite'
   end
 
-  resources :users, only: [:index,:show,:edit,:update]
 
-  resources :users do
+
+  resources :users, only: [:index, :show, :edit, :destroy] do
     resource :relationships, only: [:create, :destroy]
     get 'followings', to: 'relationships#followings', as: 'followings'
     get 'followers', to: 'relationships#followers', as: 'followers'
   end
 
   get "/search", to: "searches#search"
-  
-  scope module: :public do
-    resources :book_comments, only: [:create, :destroy, :update]
-  end
+
+  #scope module: :public do
+    #resources :book_comments, only: [:create, :destroy, :update]
+ # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
 
